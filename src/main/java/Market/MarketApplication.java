30d4ff8ed3859f43d230f11dfc787bfbf8fb4185
plagentile1121@gmail.com <plagentile1121@gmail.com>
2020-04-11 15:32:11 -0500

    Test success!

--- src/main/java/Market/MarketApplication.java
+++ src/main/java/Market/MarketApplication.java
@@ -8,6 +8,7 @@ public class MarketApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MarketApplication.class, args);
+        System.out.println("Peter was here");
     }
 
 }
