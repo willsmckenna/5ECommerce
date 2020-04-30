a1540c7c4ffc2cbb4ca8e56c6cba5b182828a0fd
salehkai999 <saleh.mk96@gmail.com>
2020-04-30 16:56:43 -0500

    Moved user repo to the repo package.

--- src/main/java/Market/Repo/UserRepository.java
+++ src/main/java/Market/Repo/UserRepository.java
@@ -1,5 +1,6 @@
-package Market.Model;
+package Market.Repo;
 
+import Market.Model.UserType;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<UserType, Long> {
