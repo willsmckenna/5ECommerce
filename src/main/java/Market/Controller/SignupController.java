be58739a151e728b876a1f99ae9adb1977c67964
salehkai999 <saleh.mk96@gmail.com>
2020-05-13 19:34:48 -0500

    Moved address repo to the repo package.

---
+++ src/main/java/Market/Controller/SignupController.java
@@ -0,0 +1,23 @@
+package Market.Controller;
+
+import Market.Repo.UsersRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SignupController {
+
+    private final UsersRepository usersRepository;
+
+    public SignupController(UsersRepository usersRepository) {
+        this.usersRepository = usersRepository;
+    }
+
+    @GetMapping("/signupview")
+    public String signup(Model model) {
+        return "signupview/signup";
+    }
+
+
+}
