75d2d7153e934105d1215ce4b3d565c2a5ef1797
plagentile1121@gmail.com <plagentile1121@gmail.com>
2020-05-14 21:44:51 -0500

    Getting ready for UI

--- src/main/java/Market/Controller/BuyerController.java
+++ src/main/java/Market/Controller/BuyerController.java
@@ -1,4 +1,14 @@
 package Market.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class BuyerController {
+
+    @RequestMapping({"/CustomerAccount", "/CustomerAccount.html", "/MyBuyerAccount", "MyBuyerAccount.html"})
+    public String getBuyerAccount()
+    {
+        return "BuyersView/BuyerHomePage";
+    }
 }
