b1ef7d9e94b67ba67e4c00d2ddf1fa7f47aabb87
plagentile1121@gmail.com <plagentile1121@gmail.com>
2020-05-18 02:59:53 -0500

    Fixed buyer and seller views

--- src/main/java/Market/Controller/SellerController.java
+++ src/main/java/Market/Controller/SellerController.java
@@ -1,6 +1,14 @@
 package Market.Controller;
 
-public class SellerController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+public class SellerController {
+    @RequestMapping({"/MerchantAccount", "/MerchantAccount.html", "/MySellerAccount", "MySellerAccount.html"})
+    public String getBuyerAccount()
+    {
+        return "SellersView/SellerHomePage";
+    }
 
 }
